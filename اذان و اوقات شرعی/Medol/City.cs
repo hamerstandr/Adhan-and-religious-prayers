e6002555e75ffbaa57d48eab360469f275b7f47b
hamed momen <hamerstandr@live.com>
2018-12-22 07:44:46 +0330

    Update UI + Bilding Data
    Update UI + Bilding Data

---
+++ اذان و اوقات شرعی/Medol/City.cs
@@ -0,0 +1,20 @@
+﻿namespace اذان_و_اوقات_شرعی
+{
+    public class City
+    {
+        private double tol;
+        private double arz;
+        private string name;
+
+        public double Tol { get => tol; set => tol = value; }
+        public double Arz { get => arz; set => arz = value; }
+        public string Name { get => name; set => name = value; }
+
+        public City(string Name, double Tol, double Arz)
+        {
+            this.Name = Name;
+            this.Tol = Tol;
+            this.Arz = Arz;
+        }
+    }
+}
---
+++ اذان و اوقات شرعی/Medol/DataView.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+namespace اذان_و_اوقات_شرعی
+{
+    public class DataView : INotifyPropertyChanged
+    {
+        private string _azansobh = "";
+
+        public string Azansobh { get => _azansobh; set { _azansobh = value; NotifyPropertyChanged("Azansobh"); } }
+        public string Tolo { get => _tolo; set { _tolo = value; NotifyPropertyChanged("Tolo"); } }
+        public string Azanzohr { get => _azanzohr; set { _azanzohr = value; NotifyPropertyChanged("Azanzohr"); } }
+        public string Gorob { get => _gorob; set { _gorob = value; NotifyPropertyChanged("Gorob"); } }
+        public string AzaneGorob { get => _azaneGorob; set { _azaneGorob = value; NotifyPropertyChanged("AzaneGorob"); } }
+
+        
+        private string _tolo;
+        private string _azanzohr;
+        private string _gorob;
+        private string _azaneGorob;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
