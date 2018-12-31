9956d3ff4b930495842bb3718dafc443e238bda4
hamed momen <hamerstandr@live.com>
2018-12-31 18:35:29 +0330

    Add Timer+Alarm
    افزودن تایمر وآلارم هنگام اذان

--- اذان و اوقات شرعی/MainWindow.xaml.cs
+++ اذان و اوقات شرعی/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using اذان_و_اوقات_شرعی.Properties;
 
 namespace اذان_و_اوقات_شرعی
@@ -23,11 +25,11 @@ namespace اذان_و_اوقات_شرعی
     {
         public DataView DataView = new DataView();
         List<City> ListCity = new Data().Citys;
-
+        readonly DispatcherTimer timer = new DispatcherTimer();
+        public DispatcherTimer Timer => timer;
         public MainWindow()
         {
             InitializeComponent();
-            
             G1.DataContext = DataView;
             G2.DataContext = DataView;
             ComboBoxCity.ItemsSource = ListCity;
@@ -35,10 +37,36 @@ namespace اذان_و_اوقات_شرعی
             ComboBoxCity.SelectionChanged += City_SelectionChanged;
             ComboBoxCity.SelectedIndex = Settings.Default.City;//qom
             //Text1.Text=SetDate();
+            player.Stream = StreamMedia;
+            Timer.Interval = TimeSpan.FromSeconds(1);
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
         }
-        
-
-private void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        DateTime Date;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Date = DateTime.Now;
+            TimeSpan time = new TimeSpan(Date.Hour, Date.Minute, Date.Second);
+            if (time== DataView.Azansobh)
+            {
+                PlaySound();
+            }
+            else if (time == DataView.Azanzohr)
+            {
+                PlaySound();
+            }
+            else if (time == DataView.AzaneGorob)
+            {
+                PlaySound();
+            }
+        }
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+        Stream StreamMedia = new MemoryStream(Properties.Resources.رحیم_موذن_زاده);
+        void PlaySound()
+        {
+            player.Play();
+        }
+        private void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             City d= ComboBoxCity.SelectedItem as City;
             Settings.Default.City = ComboBoxCity.SelectedIndex;
@@ -50,15 +78,15 @@ private void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
         void SetDate(City city)
         {
             Prayer prayer = new Prayer();
-            DateTime date = new DateTime(DateTime.Now.Year, DateTime.Now.Month,  DateTime.Now.Day,new System.Globalization.PersianCalendar());
+            //DateTime date = new DateTime(DateTime.Now.Year, DateTime.Now.Month,  DateTime.Now.Day,new System.Globalization.PersianCalendar());
             prayer.month = DateTime.Now.Month;
             byte Month = byte.Parse(DateTime.Now.Month.ToString());
             byte Day = byte.Parse(DateTime.Now.Day.ToString());
-            DataView.Azansobh = prayer.MorningPrayer(Month, Day, city.Arz, city.Tol).ToString();
-            DataView.Tolo = prayer.Sunrise(Month, (Day), city.Arz, city.Tol).ToString();
-            DataView.Azanzohr = prayer.MiddayPrayer((Month), Day, city.Arz).ToString()+"\r\n";
-            DataView.Gorob = prayer.Sunset((Month), (Day), city.Arz, city.Tol).ToString();
-            DataView.AzaneGorob = prayer.SunsetPrayer((Month), (Day), city.Arz, city.Tol).ToString();
+            DataView.Azansobh = prayer.MorningPrayer(Month, Day, city.Arz, city.Tol);
+            DataView.Tolo = prayer.Sunrise(Month, (Day), city.Arz, city.Tol);
+            DataView.Azanzohr = prayer.MiddayPrayer((Month), Day, city.Arz);
+            DataView.Gorob = prayer.Sunset((Month), (Day), city.Arz, city.Tol);
+            DataView.AzaneGorob = prayer.SunsetPrayer((Month), (Day), city.Arz, city.Tol);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -77,6 +105,9 @@ private void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
             this.Close();
         }
         bool Cansel = true;
+
+        
+
         private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (Cansel)
--- اذان و اوقات شرعی/Medol/DataView.cs
+++ اذان و اوقات شرعی/Medol/DataView.cs
@@ -1,21 +1,20 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 namespace اذان_و_اوقات_شرعی
 {
     public class DataView : INotifyPropertyChanged
     {
-        private string _azansobh = "";
+        public TimeSpan Azansobh { get => _azansobh; set { _azansobh = value; NotifyPropertyChanged("Azansobh"); } }
+        public TimeSpan Tolo { get => _tolo; set { _tolo = value; NotifyPropertyChanged("Tolo"); } }
+        public TimeSpan Azanzohr { get => _azanzohr; set { _azanzohr = value; NotifyPropertyChanged("Azanzohr"); } }
+        public TimeSpan Gorob { get => _gorob; set { _gorob = value; NotifyPropertyChanged("Gorob"); } }
+        public TimeSpan AzaneGorob { get => _azaneGorob; set { _azaneGorob = value; NotifyPropertyChanged("AzaneGorob"); } }
 
-        public string Azansobh { get => _azansobh; set { _azansobh = value; NotifyPropertyChanged("Azansobh"); } }
-        public string Tolo { get => _tolo; set { _tolo = value; NotifyPropertyChanged("Tolo"); } }
-        public string Azanzohr { get => _azanzohr; set { _azanzohr = value; NotifyPropertyChanged("Azanzohr"); } }
-        public string Gorob { get => _gorob; set { _gorob = value; NotifyPropertyChanged("Gorob"); } }
-        public string AzaneGorob { get => _azaneGorob; set { _azaneGorob = value; NotifyPropertyChanged("AzaneGorob"); } }
-
-        
-        private string _tolo;
-        private string _azanzohr;
-        private string _gorob;
-        private string _azaneGorob;
+        private TimeSpan _azansobh;
+        private TimeSpan _tolo;
+        private TimeSpan _azanzohr;
+        private TimeSpan _gorob;
+        private TimeSpan _azaneGorob;
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void NotifyPropertyChanged(string propertyName)
